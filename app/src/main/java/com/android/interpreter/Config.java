1cbdf2e202ef94691f0366b2ca91ca8dec6f7e58
Demo User <demouser@MacBook-Air-7.local>
2015-08-06 17:50:16 +0100

    Added Config class for all static fields

---
+++ app/src/main/java/com/android/interpreter/Config.java
@@ -0,0 +1,8 @@
+package com.android.interpreter;
+
+/**
+ * Created by demouser on 8/6/15.
+ */
+public class Config {
+    public static String mainFireBaseRef = "https://flickering-heat-70.firebaseio.com/interpreter/";
+}
