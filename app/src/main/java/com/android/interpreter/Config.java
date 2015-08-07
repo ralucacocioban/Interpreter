d3c70817503ba0b7c4f1c8edcff90cab7c40600c
Demo User <demouser@MacBook-Air-101.local>
2015-08-07 01:55:33 +0100

    Added map language-code to Config

--- app/src/main/java/com/android/interpreter/Config.java
+++ app/src/main/java/com/android/interpreter/Config.java
@@ -1,5 +1,7 @@
 package com.android.interpreter;
 
+import java.util.HashMap;
+
 /**
  * Created by demouser on 8/6/15.
  */
@@ -11,4 +13,12 @@ public class Config {
 
     public static final String[] sendLanguageArray = new String[]{"English", "Russian", "Ukrainian", "Polish", "Dutch", "Romanian"};
     public static final String[] receiveLanguageArray = new String[]{"English", "Russian", "Ukrainian", "Polish", "Dutch", "Romanian"};
+    public static final HashMap<String,String> langCodes = new HashMap<String, String>()
+    {{     put("English",   "en");
+                put("Russian",  "rus");
+                put("Ukrainian","uk");
+                put("Polish",   "pl");
+                put("Dutch",    "nl");
+                put("Romanian", "ro");
+            }};
 }
